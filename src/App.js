6d19a158f4c609b17537949eb2e12b2f6cc755c8
asaldivar <ag.saldivar@gmail.com>
2020-08-28 13:32:37 -0700

    change name from Jane to Alex

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <h1>
-          Hello World.
-          <p>
-            My name is Jane Lee and I'm currently a Product Manager at Ruggable. This first pilot project will consist building a basic web interface using REACT.
-          </p>
-        </h1>
+      <h1>
+        Hello World.
+        <p>
+          My name is Alex Lee and I'm currently a Product Manager at Ruggable.
+          This first pilot project will consist building a basic web interface
+          using REACT.
+        </p>
+      </h1>
     </div>
-  )
+  );
 }
 
 export default App;
